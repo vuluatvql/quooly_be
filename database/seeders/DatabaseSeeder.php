fc9293af21677377019fa6147e9a97a3d0153c33
nguyen dinh tu <dinhtu@vinayamato.com>
2022-07-13 10:37:58 +0700

    add prefecture

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(AdminSeeder::class);
+        $this->call(PrefecturesSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
